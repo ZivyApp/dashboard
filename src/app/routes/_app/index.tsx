26c72e96e689f12dc69a99917e9857f4b19991bc
Adams Alves <alves.adams@gmail.com>
2026-05-21 07:50:55 -0300

    feat(plan-6-3): rota / renderiza OverviewPage (sai redirect p/ inbox)

--- src/app/routes/_app/index.tsx
+++ src/app/routes/_app/index.tsx
@@ -1,28 +1,14 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 import { myCondosQueryOptions } from "@/features/condo/useMyCondos";
-import { getLastSelected } from "@/stores/activeCondo";
+import { OverviewPage } from "@/features/overview/OverviewPage";
 
 export const Route = createFileRoute("/_app/")({
   beforeLoad: async ({ context }) => {
     const condos = await context.queryClient.ensureQueryData(myCondosQueryOptions());
-
     if (condos.length === 0) {
       // eslint-disable-next-line @typescript-eslint/only-throw-error
       throw redirect({ to: "/no-access" });
     }
-
-    const lastId = getLastSelected();
-    const target = condos.find((c) => c.condoId === lastId) ?? condos[0];
-
-    if (!target) {
-      // eslint-disable-next-line @typescript-eslint/only-throw-error
-      throw redirect({ to: "/no-access" });
-    }
-
-    // eslint-disable-next-line @typescript-eslint/only-throw-error
-    throw redirect({
-      to: "/c/$condoId/inbox",
-      params: { condoId: target.condoId },
-    });
   },
+  component: OverviewPage,
 });
